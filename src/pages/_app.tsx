40f8a206ec66614d2e268c9b0b225ee112367f19
ONE Webdesign <info@onewebdesign.nl>
2023-04-23 13:44:01 +0200

    first commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,16 +3,22 @@ import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 
 import "@/styles/globals.css";
+import Head from "next/head";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
-  return (
+  return <>
+    <Head>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+      <link href="https://fonts.googleapis.com/css2?family=Londrina+Solid:wght@100;300;400;900&display=swap" rel="stylesheet" />
+    </Head>
     <SessionProvider session={session}>
       <Component {...pageProps} />
     </SessionProvider>
-  );
+  </>
 };
 
 export default MyApp;
